289cceec44251065b8855a4d2956e9579c684761
PetterTSaatvedt <71778703+PetterTSaatvedt@users.noreply.github.com>
2024-11-20 12:05:50 +0100

    Made education section responsive

--- src/components/EducationSection.js
+++ src/components/EducationSection.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Avatar, Heading, VStack, HStack, Mark, defineStyle, Text, Divider, Box, Image } from "@chakra-ui/react";
+import { Avatar, Heading, VStack, HStack, Mark, defineStyle, Text, Divider, Box, Image, Stack, StackDivider } from "@chakra-ui/react";
 import FullScreenSection from "./FullScreenSection";
 
 const EducationSection = () => {
@@ -18,36 +18,38 @@ const EducationSection = () => {
         color="#cad2c5"
         id="education-section"
         >
-            <VStack spacing={8}>
-                <Heading as="h1" size="xl">Utdanning</Heading>
-                <HStack spacing={8} justifyContent="center" alignItems="center">
-                    <Box>
-                        <VStack>
-                            <HStack spacing={8}>
-                                <Heading as="h2" size="lg">{firstStudyName} <br /> {firstSchoolName}</Heading>
-                                <Image src={require("../images/kristiania.png")} h="60px" rounded="md" />
-                            </HStack>
-                            <HStack spacing={16}>
-                                <Text fontSize="xl" fontStyle="italic" fontWeight="bold">Bachelor</Text>
-                                <Text fontSize="xl">2020 - 2023</Text>
-                            </HStack>
-                        </VStack>
-                    </Box>
-                    <Divider orientation="vertical" h="300px" p={2}/>
-                    <Box>
-                        <VStack>
-                            <HStack spacing={8}>
-                                <Heading as="h2" size="lg">{secondStudyName} <br /> {secondSchoolName}</Heading>
-                                <Image src={require("../images/oslomet.png")} h="60px" rounded="md" />
-                            </HStack>
-                            <HStack spacing={16}>
-                                <Text fontSize="xl" fontStyle="italic" fontWeight="bold">Master</Text>
-                                <Text fontSize="xl">2023 - 2025</Text>
-                            </HStack>
-                        </VStack>
-                    </Box>
-                </HStack>
-            </VStack>
+            <Heading as="h1" size="xl">Utdanning</Heading>
+            <Stack 
+                direction={{base: 'column', lg: 'row'}}
+                spacing={8}
+                divider={<StackDivider p={2} />}
+            >
+                <Box>
+                    <VStack>
+                        <HStack spacing={8}>
+                            <Heading as="h2" size="lg">{firstStudyName} <br /> {firstSchoolName}</Heading>
+                            <Image src={require("../images/kristiania.png")} h="60px" rounded="md" />
+                        </HStack>
+                        <HStack spacing={16}>
+                            <Text fontSize="xl" fontStyle="italic" fontWeight="bold">Bachelor</Text>
+                            <Text fontSize="xl">2020 - 2023</Text>
+                        </HStack>
+                    </VStack>
+                </Box>
+                <Box>
+                    <VStack>
+                        <HStack spacing={8}>
+                            <Heading as="h2" size="lg">{secondStudyName} <br /> {secondSchoolName}</Heading>
+                            <Image src={require("../images/oslomet.png")} h="60px" rounded="md" />
+                        </HStack>
+                        <HStack spacing={16} justifyContent="start" width="100%">
+                            <Text fontSize="xl" fontStyle="italic" fontWeight="bold">Master</Text>
+                            <Text fontSize="xl">2023 - 2025</Text>
+                        </HStack>
+                        <Text>Spesialisering i universell utforming av IKT</Text>
+                    </VStack>
+                </Box>
+            </Stack>
         </FullScreenSection>
     )
 }
