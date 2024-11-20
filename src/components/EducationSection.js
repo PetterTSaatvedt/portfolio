646a40e966a92c4560d75890121645dbcbb1b7ae
PetterTSaatvedt <71778703+PetterTSaatvedt@users.noreply.github.com>
2024-11-20 13:03:09 +0100

    Styling and layout adjustments (education)

--- src/components/EducationSection.js
+++ src/components/EducationSection.js
@@ -26,27 +26,31 @@ const EducationSection = () => {
             >
                 <Box>
                     <VStack>
-                        <HStack spacing={8}>
-                            <Heading as="h2" size="lg">{firstStudyName} <br /> {firstSchoolName}</Heading>
+                        <HStack justifyContent="space-between" width="100%" spacing={8}>
+                            <Heading as="h2" size="lg">{firstStudyName} <br />
+                                <Heading as="h2" size="lg" color="#84a98c">{firstSchoolName}</Heading>
+                            </Heading>
                             <Image src={require("../images/kristiania.png")} h="60px" rounded="md" />
                         </HStack>
-                        <HStack spacing={16}>
+                        <HStack spacing={4} justifyContent="start" width="100%" divider={<StackDivider borderColor="#2f3e46"/>}>
                             <Text fontSize="xl" fontStyle="italic" fontWeight="bold">Bachelor</Text>
-                            <Text fontSize="xl">2020 - 2023</Text>
+                            <Text fontSize="lg">2020 - 2023</Text>
                         </HStack>
                     </VStack>
                 </Box>
                 <Box>
-                    <VStack>
-                        <HStack spacing={8}>
-                            <Heading as="h2" size="lg">{secondStudyName} <br /> {secondSchoolName}</Heading>
+                    <VStack alignItems="start">
+                        <HStack justifyContent="space-between" width="100%" spacing={8}>
+                            <Heading as="h2" size="lg">{secondStudyName} <br /> 
+                                <Heading as="h2" size="lg" color="#84a98c">{secondSchoolName}</Heading>
+                            </Heading>
                             <Image src={require("../images/oslomet.png")} h="60px" rounded="md" />
                         </HStack>
-                        <HStack spacing={16} justifyContent="start" width="100%">
+                        <HStack spacing={4} divider={<StackDivider borderColor="#2f3e46"/>}>
                             <Text fontSize="xl" fontStyle="italic" fontWeight="bold">Master</Text>
-                            <Text fontSize="xl">2023 - 2025</Text>
+                            <Text fontSize="lg">2023 - 2025</Text>
+                            <Text fontSize="lg">Universell utforming av IKT</Text>
                         </HStack>
-                        <Text>Spesialisering i universell utforming av IKT</Text>
                     </VStack>
                 </Box>
             </Stack>
